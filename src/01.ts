35a81f38ba138d0a8470daccbd6e252caad93482
Nicole Chung <nicole.chung@gmail.com>
2020-12-01 07:36:46 -0500

    pretty and lint

--- src/01.ts
+++ src/01.ts
@@ -18,54 +18,56 @@ In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying the
 Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
  */
 
+import fs from 'fs';
 
-export const SUM_TO = 2020 
+export const SUM_TO = 2020;
 
-export const pairs:(nums: number[], sum: number)=>number[] = (nums, sum) => {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i+1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === sum) {
-                return [nums[i], nums[j]]
-            }
-        }
+export const pairs: (nums: number[], sum: number) => number[] = (nums, sum) => {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === sum) {
+        return [nums[i], nums[j]];
+      }
     }
-    return []
-}
+  }
+  return [];
+};
 
-export const threes:(nums: number[], sum: number)=>number[] = (nums, sum) => {
-    const sorted = nums.sort((a,b) => a-b)
-    for (let i = 0; i < sorted.length; i++) {
-        if (sorted[i] == sorted[i-1]) {
-            continue
-        }
-        for (let j = i+1; j < sorted.length; j++) {
-            if (sorted[j] === sorted[j-1]) {
-                continue
-            }
-            for (let k = j + 1; k < sorted.length; k++) {
-                if (sorted[k] === sorted[k-1]) {
-                    continue
-                }
-                if (sorted[i] + sorted[j] + sorted[k] === sum) {
-                    return [sorted[i], sorted[j], sorted[k]]
-                }
-
-            }
-            
-        }
+export const threes: (nums: number[], sum: number) => number[] = (
+  nums,
+  sum
+) => {
+  const sorted = nums.sort((a, b) => a - b);
+  for (let i = 0; i < sorted.length; i++) {
+    if (sorted[i] === sorted[i - 1]) {
+      continue;
     }
-    return [0,0,0]
-}
+    for (let j = i + 1; j < sorted.length; j++) {
+      if (sorted[j] === sorted[j - 1]) {
+        continue;
+      }
+      for (let k = j + 1; k < sorted.length; k++) {
+        if (sorted[k] === sorted[k - 1]) {
+          continue;
+        }
+        if (sorted[i] + sorted[j] + sorted[k] === sum) {
+          return [sorted[i], sorted[j], sorted[k]];
+        }
+      }
+    }
+  }
+  return [0, 0, 0];
+};
 
-export const multiply= ([...numbers]) => {
-    return numbers.reduce((a,b) => a*b)
-}
-
-import fs from 'fs'
+export const multiply = ([...numbers]) => {
+  return numbers.reduce((a, b) => a * b);
+};
 
 const input = fs.readFileSync(`${__dirname}/01.txt`, 'utf8');
 
-export const numbers:number[] = input.split('\n').map(str => parseInt(str, 10))
+export const numbers: number[] = input
+  .split('\n')
+  .map(str => parseInt(str, 10));
 
-export const answer = multiply([...pairs(numbers, SUM_TO)])
-export const followUp = multiply([...threes(numbers, SUM_TO)])
+export const answer = multiply([...pairs(numbers, SUM_TO)]);
+export const followUp = multiply([...threes(numbers, SUM_TO)]);
--- test/01.test.ts
+++ test/01.test.ts
@@ -1,11 +1,26 @@
-import { pairs, threes, multiply, numbers, answer, followUp, SUM_TO } from '../src/01';
+import {
+  pairs,
+  threes,
+  multiply,
+  numbers,
+  answer,
+  followUp,
+  SUM_TO,
+} from '../src/01';
 
 describe('Day one tests', () => {
   it('pairs finds two numbers that add up to 2020', () => {
-    expect(pairs([1721, 979, 366, 299, 675, 1456], SUM_TO)).toEqual([1721, 299]);
+    expect(pairs([1721, 979, 366, 299, 675, 1456], SUM_TO)).toEqual([
+      1721,
+      299,
+    ]);
   });
   it('threes finds three numbers that sum to 2020', () => {
-    expect(threes([1721, 979, 366, 299, 675, 1456], SUM_TO)).toEqual([366, 675, 979]);
+    expect(threes([1721, 979, 366, 299, 675, 1456], SUM_TO)).toEqual([
+      366,
+      675,
+      979,
+    ]);
   });
   it('threes returns empty array for numbers that do not sum to 2020', () => {
     expect(threes([1, 2, 3, 4, 5], SUM_TO)).toEqual([0, 0, 0]);
