a26ad53a312fbd82f09c31ee89ce5ad29bfbebcc
Nicole Chung <nicole.chung@gmail.com>
2020-12-03 07:55:25 -0500

    basic test passing

--- test/02.test.ts
+++ test/02.test.ts
@@ -1,10 +1,19 @@
-import { howManyValid } from '../src/02'
+import { howManyValid, isValid, Input } from '../src/02'
 
 describe('Day 2', () => {
-    const input = [
-        [[1,3], 'a', 'abcde'],
-        [[1,3], 'b', 'cdefg'],
-        [[2,9], 'c', 'ccccccccc']
-    ]
-    expect(howManyValid(input)).toEqual(2)
+    it('isValid is true with the correct input', () => {
+        expect(isValid([1,3, 'a', 'abcde'])).toEqual(true)
+    })
+    it('isValid is false with the correct input', () => {
+        expect(isValid([1,3, 'b', 'cdefg'])).toEqual(false)
+    })
+    it('howManyValid returns the correct number of valid', () => {
+        const inputs:Input[] = [
+            [1,3, 'a', 'abcde'],
+            [1,3, 'b', 'cdefg'],
+            [2,9, 'c', 'ccccccccc']
+        ]
+        expect(howManyValid(inputs)).toEqual(2)
+    })
+    
 })
